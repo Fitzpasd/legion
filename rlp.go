be35167b85d340af50bad6473ca1bf06b8925bd4
Shane Fitzpatrick <fitzpasd@gmail.com>
2022-09-09 14:50:28 -0700

    Basic RLP encode function

---
+++ rlp.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+func getLengthInBytes(length int) byte {
+	binaryLength := len(strconv.FormatInt(int64(length), 2))
+	return byte(math.Ceil(float64(binaryLength) / 8))
+}
+
+func encodeString(data string) []byte {
+	var strLen = len(data)
+
+	var output []byte
+
+	switch {
+	case strLen == 1 && data[0] < 0x80:
+		return append(output, data[0])
+
+	case strLen < 56:
+		output = append(output, 0x80+byte(strLen))
+		return append(output, data...)
+
+	default:
+		output = append(output, 0xb7+getLengthInBytes(strLen))
+		output = append(output, byte(strLen))
+		return append(output, data...)
+	}
+}
+
+func encodeList(data any) ([]byte, error) {
+	items := reflect.ValueOf(data)
+
+	if items.Len() == 0 {
+		return []byte{0xc0}, nil
+	}
+
+	allBytes := true
+	for i := 0; i < items.Len(); i++ {
+		if _, ok := items.Index(i).Interface().(byte); !ok {
+			allBytes = false
+			break
+		}
+	}
+
+	if allBytes {
+		var sb strings.Builder
+
+		for i := 0; i < items.Len(); i++ {
+			sb.WriteByte(items.Index(i).Interface().(byte))
+		}
+
+		return encodeString(sb.String()), nil
+	} else {
+		var output []byte
+
+		for i := 0; i < items.Len(); i++ {
+			item := items.Index(i)
+			blah, err := Encode(item.Interface())
+
+			if err != nil {
+				return nil, err
+			} else {
+				output = append(output, blah...)
+			}
+		}
+
+		var outputLen = len(output)
+		switch {
+		case outputLen < 56:
+			return append([]byte{0xc0 + byte(len(output))}, output...), nil
+
+		default:
+			return append([]byte{0xf7 + getLengthInBytes(outputLen), byte(outputLen)}, output...), nil
+		}
+	}
+}
+
+func Encode(data any) ([]byte, error) {
+	isList := false
+
+	switch t := data.(type) {
+	case string:
+		return encodeString(data.(string)), nil
+	case byte:
+		return encodeString(string(data.(byte))), nil
+	case []byte:
+		if len(data.([]byte)) == 0 {
+			return []byte{0xc0}, nil
+		}
+		return encodeString(string(data.([]byte))), nil
+	case []any, []string:
+		isList = true
+
+	case any:
+		items := reflect.ValueOf(data)
+		isList = items.Kind() == reflect.Array || items.Kind() == reflect.Slice
+
+	default:
+		return nil, errors.New(fmt.Sprintf("Unsupported type %s", t))
+	}
+
+	if isList {
+		return encodeList(data)
+	} else {
+		return nil, errors.New(fmt.Sprintf("Unexpected value %s", data))
+	}
+}
---
+++ rlp_test.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func testHelper(actual, expected []byte, t *testing.T) {
+	if !bytes.Equal(actual, expected) {
+		t.Errorf("\nActual:{[% x]}\nExpected:{[% x]}", actual, expected)
+	}
+}
+
+func encodeAndIgnoreError(data any) []byte {
+	encoded, _ := Encode(data)
+	return encoded
+}
+
+func TestEncodeEmptyString(t *testing.T) {
+	var asString string = ""
+	var asInterface interface{} = ""
+	expected := []byte{0x80}
+
+	testHelper(encodeAndIgnoreError(asString), expected, t)
+	testHelper(encodeAndIgnoreError(asInterface), expected, t)
+}
+
+func TestEncodeNonEmptyString(t *testing.T) {
+	testHelper(encodeAndIgnoreError("dog"), []byte{0x83, 'd', 'o', 'g'}, t)
+	testHelper(encodeAndIgnoreError([]byte("dog")), []byte{0x83, 'd', 'o', 'g'}, t)
+	testHelper(
+		encodeAndIgnoreError("Lorem ipsum dolor sit amet, consectetur adipisicing elit"),
+		[]byte{0xb8, 0x38, 'L', 'o', 'r', 'e', 'm', ' ', 'i', 'p', 's', 'u',
+			'm', ' ', 'd', 'o', 'l', 'o', 'r', ' ', 's', 'i', 't', ' ', 'a',
+			'm', 'e', 't', ',', ' ', 'c', 'o', 'n', 's', 'e', 'c', 't', 'e',
+			't', 'u', 'r', ' ', 'a', 'd', 'i', 'p', 'i', 's', 'i', 'c', 'i',
+			'n', 'g', ' ', 'e', 'l', 'i', 't'}, t)
+}
+
+func TestEncodeStringList(t *testing.T) {
+	actual := encodeAndIgnoreError([]string{"cat", "dog"})
+	expected := []byte{0xc8, 0x83, 'c', 'a', 't', 0x83, 'd', 'o', 'g'}
+	testHelper(actual, expected, t)
+}
+
+func TestEncodeEmptyList(t *testing.T) {
+	expected := []byte{0xc0}
+	testHelper(encodeAndIgnoreError([]string{}), expected, t)
+	testHelper(encodeAndIgnoreError([]byte{}), expected, t)
+	testHelper(encodeAndIgnoreError([]interface{}{}), expected, t)
+}
+
+func TestSetTheoreticalRepresentation(t *testing.T) {
+	actual := encodeAndIgnoreError([]interface{}{
+		[]interface{}{},
+		[][]interface{}{{}},
+		[]interface{}{[]string{}, [][]interface{}{{}}},
+	})
+
+	expected := []byte{0xc7, 0xc0, 0xc1, 0xc0, 0xc3, 0xc0, 0xc1, 0xc0}
+
+	testHelper(actual, expected, t)
+}
