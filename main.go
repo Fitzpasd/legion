2cad7795b7555da7127b4a4ffb80a0db3ec0805d
Shane Fitzpatrick <fitzpasd@gmail.com>
2022-09-14 13:41:48 -0700

    Start up server during main.go

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	// Parse command line flags
+	serverAddress := flag.String("ip", "0.0.0.0:0", "IP:Port for the server")
+	flag.Parse()
+
+	// Start local node and server
+	localNode, _ := NewLocalNode()
+	server, _ := NewServer(*serverAddress, localNode)
+	server.Start()
+
+	// Write ping to bootnode
+	bootNode := GetBootNode()
+	server.WritePing(&bootNode)
+
+	select {}
 }
